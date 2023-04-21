d284f66fb2ca5e13bec15457821e720ceaec6923
Parth971 <desaiparth971@gmail.com>
2023-04-22 00:24:26 +0530

    feat: Create change password api.

--- web/accounts/views.py
+++ web/accounts/views.py
@@ -1,5 +1,5 @@
 from rest_framework import status
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, UpdateAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -8,7 +8,7 @@ from accounts.models import User
 
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from accounts.serializers import LoginSerializer, RegisterSerializer, ForgotPasswordSerializer
+from accounts.serializers import LoginSerializer, RegisterSerializer, ForgotPasswordSerializer, ChangePasswordSerializer
 
 
 class RegisterView(CreateAPIView):
@@ -30,3 +30,9 @@ class ForgotPasswordView(APIView):
         serializer.is_valid(raise_exception=True)
         # send_forgot_password_email()
         return Response(data={'message': 'Restore password link has been sent to email.'}, status=status.HTTP_200_OK)
+
+
+class ChangePasswordView(UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = ChangePasswordSerializer
+    lookup_url_kwarg = 'user_id'
