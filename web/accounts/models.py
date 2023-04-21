853f716366564a3d1b99185f355b5d40f34d15fb
Parth971 <desaiparth971@gmail.com>
2023-04-22 00:24:27 +0530

    refactor: Add ActivateUserToken model
    - Add new model named ActivateUserToken in accounts app to save email verification tokens.

--- web/accounts/models.py
+++ web/accounts/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -20,5 +22,29 @@ class User(AbstractUser):
     def get_user(cls, query):
         return cls.objects.filter(**query).first()
 
+    def activate(self):
+        self.is_active = True
+        self.save()
+
+    def generate_activation_token(self):
+        token = uuid.uuid4()
+        ActivateUserToken.objects.create(token=token, user=self)
+        return token
+
     def __str__(self):
         return self.email
+
+
+class ActivateUserToken(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    token = models.CharField(max_length=36, unique=True)
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return self.user.email
+
+    def delete_token(self):
+        self.__class__.objects.filter(user=self.user).delete()
